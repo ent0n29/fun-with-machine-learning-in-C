4151d641ade3f0fde558947c1b4d3a47790d5bf6
ent0n29 <[stanoantonio29@gmail.com]>
2023-05-17 18:27:59 +0200

    first model

--- main.c
+++ main.c
@@ -1,7 +1,56 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+//building the model
+
+float train[][2] = {
+    {0,0},
+    {1,2},
+    {2,4},
+    {3,6},
+    {4,8},
+};
+
+#define train_count (sizeof(train)/sizeof(train[0]))
+
+float rand_float(void)
+{
+    return (float) rand()/ (float) RAND_MAX;
+}
+
+//this is called "cost function"
+
+float cost(float w){
+   float result = 0.0f;
+    for (size_t i = 0; i < train_count; ++i){
+        float x = train[i][0];
+        float y = x*w;
+        float d = y - train[i][1];
+        result += d*d;
+    } 
+    result /= train_count;
+    return result;
+}
 
 int main()
 {
-    printf("Hello, World\n");
+    //srand(time(0));
+    srand(69);
+    float w = rand_float()*10.0f;
+    float eps = 1e-3;
+
+    printf("%f\n", cost(w));
+    //here the model is getting worse
+    printf("%f\n", cost(w + eps));
+    //let's try another direction
+    printf("%f\n", cost(w - eps));
+    //here it's getting better and better!
+    printf("%f\n", cost(w - eps*2));
+
+    /* we need to differenciate the cost function */
+
+    
+
     return 0;
 }
