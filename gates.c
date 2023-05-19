3e397e7f89a74e5da179bfcdb2fa0559db8c1553
ent0n29 <[stanoantonio29@gmail.com]>
2023-05-19 16:49:22 +0200

    added sigmoid, with OR-gate

--- gates.c
+++ gates.c
@@ -30,7 +30,7 @@ float cost(float w1, float w2){
     for (size_t i = 0; i < train_count; ++i){
         float x1 = train[i][0];
         float x2 = train[i][1];
-        float y = x1*w1 + x2*w2;
+        float y = sigmoidf(x1*w1 + x2*w2);
         float d = y - train[i][2];
         result += d*d;
     } 
@@ -43,9 +43,9 @@ int main(void)
     srand(time(0));
     float eps = 1e-2;
     float rate = 1e-2;
-    float w1 = rand_float();
-    float w2 = rand_float();
-for (size_t i = 0; i < 1000*1000 ; ++i){
+    float w1 = rand_float()*10 - 5;
+    float w2 = rand_float()*10 - 5;
+for (size_t i = 0; i < 1 ; ++i){
     float c = cost(w1, w2);
     printf("w1 = %f, w2 = %f, c = %f\n", w1, w2, c);
     float dw1 = (cost(w1+ eps, w2) - c)/eps;
@@ -53,6 +53,13 @@ for (size_t i = 0; i < 1000*1000 ; ++i){
     w1 -= rate*dw1;
     w2 -= rate*dw2;
 }
+printf("w1 = %f, w2 = %f, c = %f\n", w1, w2, cost(w1, w2));
+
+for (size_t i = 0; i < 2; ++i) {
+        for (size_t j = 0; j < 2; ++j) {
+            printf("%zu | %zu = %f\n", i, j, sigmoidf(i*w1 + j*w2));
+        }
+    }
 
 return 0;
 
