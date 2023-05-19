bd67c8bf200ea705c63b1827a46167737426aed4
ent0n29 <[stanoantonio29@gmail.com]>
2023-05-19 18:29:37 +0200

    OR,AND & NAND gates

--- gates.c
+++ gates.c
@@ -9,17 +9,41 @@ float sigmoidf(float x)
     return 1.f / (1.f + expf(-x));
 }
 
-// implementing an OR-gate
-float train[][3] = {
+typedef float sample[3];
+
+//OR, AND, NAND gates, all of them are modelable with a single neuron
+sample or_train[] = {
     {0,0,0},
     {1,0,1},
     {0,1,1},
     {1,1,1},
 };
+sample and_train[] = {
+    {0,0,0},
+    {1,0,0},
+    {0,1,0},
+    {1,1,1},
+};
 
-#define train_count (sizeof(train)/sizeof(train[0]))
+sample nand_train[] = {
+    {0,0,1},
+    {1,0,1},
+    {0,1,1},
+    {1,1,0},    
+};
 
+sample *train = nand_train;
+size_t train_count = 4;
 
+//XOR-gate using only AND, NAND, OR
+// (x|y) & ~(x&y),  where ~ = NOT
+/*
+for (size_t x = 0; x < 2; ++x){
+    for (size_t y = 0; y < 2; ++y){
+        printf("%zu ^ %zu = %zu\n", x, y, (x|y) & (~(x&y)));
+    }
+}
+*/
 
 float rand_float(void)
 {
@@ -44,15 +68,15 @@ float cost(float w1, float w2, float b){
 int main(void)
 {
     srand(time(0));
-    float eps = 1e-2;
-    float rate = 1e-2;
+    float eps = 1e-1;
+    float rate = 1e-1 ;
     float w1 = rand_float();
     float w2 = rand_float();
     float b  = rand_float();
 for (size_t i = 0; i < 1000*1000 ; ++i){
     float c = cost(w1, w2, b);
     //printf("%f\n",c);
-    printf("w1 = %f, w2 = %f, b = %f, c = %f\n", w1, w2, b, c);
+    //printf("w1 = %f, w2 = %f, b = %f, c = %f\n", w1, w2, b, c);
     float dw1 = (cost(w1 + eps, w2, b) - c)/eps;
     float dw2 = (cost(w1, w2 + eps, b) - c)/eps;
     float db  = (cost(w1, w2, b + eps) - c)/eps;
@@ -60,7 +84,7 @@ for (size_t i = 0; i < 1000*1000 ; ++i){
     w2 -= rate*dw2;
     b  -= rate*db;
 }
-printf("w1 = %f, w2 = %f, b = %f, c = %f\n", w1, w2, b, cost(w1, w2, b));
+//printf("w1 = %f, w2 = %f, b = %f, c = %f\n", w1, w2, b, cost(w1, w2, b));
 
 //this is the forwarding (forward function)
 
