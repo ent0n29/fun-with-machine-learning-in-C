07565f499c4ee9d2348c8912e478e2fa5955d0d6
ent0n29 <[stanoantonio29@gmail.com]>
2023-05-19 17:05:25 +0200

    now the model works like an OR-gate after the training

--- gates.c
+++ gates.c
@@ -19,18 +19,21 @@ float train[][3] = {
 
 #define train_count (sizeof(train)/sizeof(train[0]))
 
+
+
 float rand_float(void)
 {
     return (float) rand()/ (float) RAND_MAX;
 }
 
 //cost function
-float cost(float w1, float w2){
+//added bias "b" to the cost func
+float cost(float w1, float w2, float b){
    float result = 0.0f;
     for (size_t i = 0; i < train_count; ++i){
         float x1 = train[i][0];
         float x2 = train[i][1];
-        float y = sigmoidf(x1*w1 + x2*w2);
+        float y = sigmoidf(x1*w1 + x2*w2 + b);
         float d = y - train[i][2];
         result += d*d;
     } 
@@ -43,21 +46,24 @@ int main(void)
     srand(time(0));
     float eps = 1e-2;
     float rate = 1e-2;
-    float w1 = rand_float()*10 - 5;
-    float w2 = rand_float()*10 - 5;
-for (size_t i = 0; i < 1 ; ++i){
-    float c = cost(w1, w2);
-    printf("w1 = %f, w2 = %f, c = %f\n", w1, w2, c);
-    float dw1 = (cost(w1+ eps, w2) - c)/eps;
-    float dw2 = (cost(w1, w2 + eps) - c)/eps;
+    float w1 = rand_float();
+    float w2 = rand_float();
+    float b  = rand_float();
+for (size_t i = 0; i < 1000*1000 ; ++i){
+    float c = cost(w1, w2, b);
+    printf("w1 = %f, w2 = %f, b = %f, c = %f\n", w1, w2, b, c);
+    float dw1 = (cost(w1 + eps, w2, b) - c)/eps;
+    float dw2 = (cost(w1, w2 + eps, b) - c)/eps;
+    float db  = (cost(w1, w2, b + eps) - c)/eps;
     w1 -= rate*dw1;
     w2 -= rate*dw2;
+    b  -= rate*db;
 }
-printf("w1 = %f, w2 = %f, c = %f\n", w1, w2, cost(w1, w2));
+printf("w1 = %f, w2 = %f, b = %f, c = %f\n", w1, w2, b, cost(w1, w2, b));
 
 for (size_t i = 0; i < 2; ++i) {
         for (size_t j = 0; j < 2; ++j) {
-            printf("%zu | %zu = %f\n", i, j, sigmoidf(i*w1 + j*w2));
+            printf("%zu | %zu = %f\n", i, j, sigmoidf(i*w1 + j*w2 + b));
         }
     }
 
