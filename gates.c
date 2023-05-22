11bb29d72c02bf0582017550ee709c9eabf0dfff
ent0n29 <[stanoantonio29@gmail.com]>
2023-05-22 14:41:59 +0200

    added gradient descent

--- gates.c
+++ gates.c
@@ -66,37 +66,70 @@ float cost(float w1, float w2, float b){
     return result;
 }
 
+void dcost(float eps,
+           float w1, float w2, float b,
+           float *dw1, float *dw2, float *db)
+{
+    float c = cost(w1, w2, b);
+    *dw1 = (cost(w1 + eps, w2, b) - c)/eps;
+    *dw2 = (cost(w1, w2 + eps, b) - c)/eps;
+    *db  = (cost(w1, w2, b + eps) - c)/eps;
+}
+
+void gcost(float w1, float w2, float b,
+           float *dw1, float *dw2, float *db)
+{
+    *dw1 = 0;
+    *dw2 = 0;
+    *db = 0;
+    size_t n = train_count;
+    for (size_t i = 0; i < n; ++i) {
+        float xi = train[i][0];
+        float yi = train[i][1];
+        float zi = train[i][2];
+        float ai = sigmoidf(xi*w1 + yi*w2 + b);
+        float di = 2*(ai - zi)*ai*(1 - ai);
+        *dw1 += di*xi;
+        *dw2 += di*yi;
+        *db  += di;
+    }
+    *dw1 /= n;
+    *dw2 /= n;
+    *db /= n;
+}
+
 int main(void)
 {
     srand(time(0));
-    float eps = 1e-1;
-    float rate = 1e-1 ;
     float w1 = rand_float();
     float w2 = rand_float();
     float b  = rand_float();
-for (size_t i = 0; i < 1000*1000 ; ++i){
-    float c = cost(w1, w2, b);
-    //printf("%f\n",c);
-    //printf("w1 = %f, w2 = %f, b = %f, c = %f\n", w1, w2, b, c);
-    float dw1 = (cost(w1 + eps, w2, b) - c)/eps;
-    float dw2 = (cost(w1, w2 + eps, b) - c)/eps;
-    float db  = (cost(w1, w2, b + eps) - c)/eps;
-    w1 -= rate*dw1;
-    w2 -= rate*dw2;
-    b  -= rate*db;
-}
-//printf("w1 = %f, w2 = %f, b = %f, c = %f\n", w1, w2, b, cost(w1, w2, b));
 
-//this is the forwarding (forward function)
+    float rate = 1e-1;
 
-for (size_t i = 0; i < 2; ++i) {
+    for (size_t i = 0; i < 100*1000; ++i) {
+        float c = cost(w1, w2, b);
+        printf("c = %f, w1 = %f, w2 = %f, b = %f\n", c, w1, w2, b);
+
+        float dw1, dw2, db;
+#if 1
+        float eps = 1e-1;
+        dcost(eps, w1, w2, b, &dw1, &dw2, &db);
+#else
+        gcost(w1, w2, b, &dw1, &dw2, &db);
+#endif
+        w1 -= rate*dw1;
+        w2 -= rate*dw2;
+        b  -= rate*db;
+    }
+    printf("c = %f, w1 = %f, w2 = %f, b = %f\n", cost(w1, w2, b), w1, w2, b);
+
+    for (size_t i = 0; i < 2; ++i) {
         for (size_t j = 0; j < 2; ++j) {
             printf("%zu | %zu = %f\n", i, j, sigmoidf(i*w1 + j*w2 + b));
         }
     }
 
-
-return 0;
-
+    return 0;
 
 }
